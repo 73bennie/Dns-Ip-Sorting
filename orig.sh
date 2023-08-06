19a5ece69d3603abe7a3eab5c2146e708fefecce
73bennie <140846710+73bennie@users.noreply.github.com>
2023-08-06 08:49:22 -0700

    Create orig.sh

---
+++ orig.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# accepts list of domains one per line on standard input, emits unique A records for said domains (assuming no errors, etc).
+
+( while read domain; do
+  dig +short A "$domain"done ) | sort -u

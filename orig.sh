75a0516721c87792b9d9c01a1a4683441c1ff825
73bennie <140846710+73bennie@users.noreply.github.com>
2023-08-06 15:53:39 -0700

    Update orig.sh
    corrected missing ';' before 'done' statement

--- orig.sh
+++ orig.sh
@@ -3,4 +3,4 @@
 # accepts list of domains one per line on standard input, emits unique A records for said domains (assuming no errors, etc).
 
 ( while read domain; do
-  dig +short A "$domain"done ) | sort -u
+  dig +short A "$domain"; done ) | sort -u
